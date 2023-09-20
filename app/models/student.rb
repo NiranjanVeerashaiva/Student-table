23630db9400541b63cc66e0788bc88836f558452
NiranjanVeerashaiva <131347934+NiranjanVeerashaiva@users.noreply.github.com>
2023-09-20 17:43:48 +0530

    Order updated

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ApplicationRecord
     validates :name, presence: true
     validates :place, presence: true
 
-    scope :sorted, ->{ order(publieshed_at: :desc)} 
+    scope :sorted, ->{ order(arel_table[:publieshed_at].desc.nulls_last).order(updated_at: :desc) } 
     scope :draft, -> { where(publieshed_at:nil) }
     scope :publieshed, -> { where("publieshed_at <= ?", Time.current) }
     scope :scheduled, -> { where("publieshed_at > ?", Time.current) }
