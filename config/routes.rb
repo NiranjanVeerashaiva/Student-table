af5cb9445ded773b04cfe9397b47b15eecf4701d
NiranjanVeerashaiva <131347934+NiranjanVeerashaiva@users.noreply.github.com>
2023-09-21 20:35:20 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # resources :student
-  get "/student/new", to: "student#new", as: :new_student
-  get "/student/:id", to: "student#show", as: :student
-  patch "/student/:id", to: "student#update"
-  delete "/student/:id", to: "student#delete"
-  get "/student/:id/edit", to: "student#edit", as: :edit_student
-  post "/student", to: "student#create", as: :students
+   resources :student
+  # get "/student/new", to: "student#new", as: :new_student
+  # get "/student/:id", to: "student#show", as: :student
+  # patch "/student/:id", to: "student#update"
+  # delete "/student/:id", to: "student#delete"
+  # get "/student/:id/edit", to: "student#edit", as: :edit_student
+  # post "/student", to: "student#create", as: :students
   
   # Defines the root path route ("/")
    root "student#index"
