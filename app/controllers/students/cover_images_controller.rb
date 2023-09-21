ad7e18ffdfa3b661e482a7ac0309ac752434a31a
NiranjanVeerashaiva <131347934+NiranjanVeerashaiva@users.noreply.github.com>
2023-09-21 21:15:55 +0530

    Add files via upload

---
+++ app/controllers/students/cover_images_controller.rb
@@ -0,0 +1,15 @@
+class Student::CoverImagesController<ApplicationController
+    include ActionView::RecordIdentifier
+    before_action :authenticate_user!
+    before_action :set_student
+    def destroy
+        @student.cover_image.purge_later
+       respond_to do |format|
+        format.html { redirect_to edit_student_path(@student) }
+        format.turbo_stream{render turbo_stream: turbo_stream.remove(dom_id(@student, :cover_image))}
+       end 
+    end
+    def set_student
+        @student=Student.find(params[:student_id])
+    end
+end
