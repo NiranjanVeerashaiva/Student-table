d2dbce612bb1530cb2b296855e0a421d25096d25
NiranjanVeerashaiva <131347934+NiranjanVeerashaiva@users.noreply.github.com>
2023-09-21 21:16:48 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-   resources :students
+   resources :students do
+    resource :cover_image, only: [:destroy], module: :student
+   end
   # get "/student/new", to: "student#new", as: :new_student
   # get "/student/:id", to: "student#show", as: :student
   # patch "/student/:id", to: "student#update"
